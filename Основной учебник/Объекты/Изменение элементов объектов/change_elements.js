ed0983faafcde020336a0feea1316e0695009851
Alexandrov Dmitriy <DemonDED@inbox.ru>
2023-04-02 00:35:06 +0300

    objects 20%

---
+++ Основной учебник/Объекты/Изменение элементов объектов/change_elements.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let obj = {
+  x: 1,
+  y: 2,
+  z: 3
+};
+
+obj.x **= 2;
+obj.y **= 2;
+obj.z **= 2;
+console.log(obj);
+
+let obj2 = {};
+
+obj2.one = 'hello';
+obj2.two = 'world';
+obj2.three = '!';
+console.log(obj2);
---
+++ Основной учебник/Объекты/Основы объектов/basics_obj.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let weekObj = {
+  1: 'monday',
+  2: 'tuesday',
+  3: 'wednsday',
+  4: 'thursday',
+  5: 'friday',
+  6: 'saturday',
+  7: 'sunday'
+};
+console.log(weekObj);
+
+let moonthObj = {
+  1: 'january',
+  2: 'february',
+  3: 'april',
+  4: 'march',
+  5: 'may',
+  6: 'june',
+  7: 'july',
+  8: 'august',
+  9: 'september',
+  10: 'october',
+  11: 'november',
+  12: 'december'
+};
+console.log(moonthObj);
+
+let userObj = {
+  name: 'Dmitry',
+  surname: 'Ivanov',
+  patronymic: 'Olegovich'
+};
+alert(`${userObj['name']} ${userObj['surname']} ${userObj['patronymic']}`);
+
